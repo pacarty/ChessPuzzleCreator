b590ed79a33b80111e9a3f08c3bd589e1e67f903
pacarty <carty.patrick@gmail.com>
2021-11-06 13:11:22 +1100

    Create new solution and delete old one
    New plan to create site using asp net core mvc with identity

---
+++ src/ChessPuzzleCreator/ChessPuzzleCreator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChessPuzzleCreator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- src/Puzzle/Models/ExampleModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Puzzle.Models
-{
-    public class ExampleModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
